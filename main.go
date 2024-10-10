f9b64bc074b843a56e6a7005aaf4e3733e4bd190
AmirH21 <amirhabsari01@gmail.com>
2024-10-10 18:44:19 +0330

    combination of for and switch

--- main.go
+++ main.go
@@ -7,39 +7,30 @@ import (
 	"github.com/AmirH21/puppy"
 )
 
-var x, y = rand.Intn(10), rand.Intn(10)
-
-func init() {
-	fmt.Printf("X is %v\nY is %v\n", x, y)
-}
-
 func main() {
 	fmt.Println(puppy.Woof())
 	fmt.Println(puppy.Woofs())
 	fmt.Println(puppy.Dog())
 	fmt.Println(puppy.Dogs())
 
-	/*
-		switch {
-		case x < 4 && y < 4:
-			fmt.Println("They are both less than 4")
+	for i := 1; i <= 42; i++ {
 
-		case x > 6 && y > 6:
-			fmt.Println("They are both greater than 6")
+		fmt.Printf("This is iteration number %v\n", i)
+		ranum := rand.Intn(5)
 
-		case x >= 4 && x <= 6:
-			fmt.Println("X is more than or equal to 4 and less than or equal to 6")
-
-		case y != 5:
-			fmt.Println("Y is not 5")
-
-		default:
-			fmt.Println("None of them happened")
+		switch ranum {
+		case 0:
+			fmt.Println("It's 0")
+		case 1:
+			fmt.Println("It's 1")
+		case 2:
+			fmt.Println("It's 2")
+		case 3:
+			fmt.Println("It's 3")
+		case 4:
+			fmt.Println("It's 4")
 		}
-	*/
-
-	for i := 0; i < 100; i++ {
-		fmt.Println(i)
+		fmt.Println("")
 	}
 
 }
