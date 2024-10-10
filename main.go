6ec9381b1212e6613dc286dc1abd919d13b58ff9
AmirH21 <amirhabsari01@gmail.com>
2024-10-10 18:09:37 +0330

    today i was working on control flow, this version uses switch

--- main.go
+++ main.go
@@ -2,13 +2,38 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 
 	"github.com/AmirH21/puppy"
 )
+
+var x, y = rand.Intn(10), rand.Intn(10)
+
+func init() {
+	fmt.Printf("X is %v\nY is %v\n", x, y)
+}
 
 func main() {
 	fmt.Println(puppy.Woof())
 	fmt.Println(puppy.Woofs())
 	fmt.Println(puppy.Dog())
 	fmt.Println(puppy.Dogs())
+
+	switch {
+	case x < 4 && y < 4:
+		fmt.Println("They are both less than 4")
+
+	case x > 6 && y > 6:
+		fmt.Println("They are both greater than 6")
+
+	case x >= 4 && x <= 6:
+		fmt.Println("X is more than or equal to 4 and less than or equal to 6")
+
+	case y != 5:
+		fmt.Println("Y is not 5")
+
+	default:
+		fmt.Println("None of them happened")
+	}
+
 }
