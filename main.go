77755b1ad7641026bbe8cda5234f76ccaf7737a6
AmirH21 <amirhabsari01@gmail.com>
2024-10-10 18:26:45 +0330

    prints every number from 0 to 99

--- main.go
+++ main.go
@@ -19,21 +19,27 @@ func main() {
 	fmt.Println(puppy.Dog())
 	fmt.Println(puppy.Dogs())
 
-	switch {
-	case x < 4 && y < 4:
-		fmt.Println("They are both less than 4")
+	/*
+		switch {
+		case x < 4 && y < 4:
+			fmt.Println("They are both less than 4")
 
-	case x > 6 && y > 6:
-		fmt.Println("They are both greater than 6")
+		case x > 6 && y > 6:
+			fmt.Println("They are both greater than 6")
 
-	case x >= 4 && x <= 6:
-		fmt.Println("X is more than or equal to 4 and less than or equal to 6")
+		case x >= 4 && x <= 6:
+			fmt.Println("X is more than or equal to 4 and less than or equal to 6")
 
-	case y != 5:
-		fmt.Println("Y is not 5")
+		case y != 5:
+			fmt.Println("Y is not 5")
 
-	default:
-		fmt.Println("None of them happened")
+		default:
+			fmt.Println("None of them happened")
+		}
+	*/
+
+	for i := 0; i < 100; i++ {
+		fmt.Println(i)
 	}
 
 }
