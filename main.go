d92cbb72dc0ba4c1a5032daf07a4e1f3c406b0ad
AmirH21 <amirhabsari01@gmail.com>
2024-10-10 18:56:48 +0330

    infinite loop

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 
 	"github.com/AmirH21/puppy"
 )
@@ -13,24 +12,8 @@ func main() {
 	fmt.Println(puppy.Dog())
 	fmt.Println(puppy.Dogs())
 
-	for i := 1; i <= 42; i++ {
-
-		fmt.Printf("This is iteration number %v\n", i)
-		ranum := rand.Intn(5)
-
-		switch ranum {
-		case 0:
-			fmt.Println("It's 0")
-		case 1:
-			fmt.Println("It's 1")
-		case 2:
-			fmt.Println("It's 2")
-		case 3:
-			fmt.Println("It's 3")
-		case 4:
-			fmt.Println("It's 4")
-		}
-		fmt.Println("")
+	for {
+		fmt.Println("Press ctrl + c")
 	}
 
 }
